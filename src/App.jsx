c55078c0e87d650a44bc23bb0713df521589d82b
mohd-jaffa <jafakash.14@gmail.com>
2025-10-16 12:56:00 +0530

    installed shadcn-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
     return (
         <>
             <h1>My app</h1>
+            <div className="flex min-h-svh flex-col items-center justify-center">
+                <Button onClick={() => window.alert("hello world!")}>
+                    Click me
+                </Button>
+            </div>
         </>
     );
 }
